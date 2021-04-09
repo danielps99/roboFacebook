d3aa6e0b18ccfe214192afd634a7364034ec4e63
Daniel <danielps99@gmail.com>
2021-04-09 19:51:30 -0300

    JFileChooser para escolher arquivo contas.json ao inves de carrega-lo fixo

--- src/main/java/service/ReaderJsonService.java
+++ src/main/java/service/ReaderJsonService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dto.ContaFacebook;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -12,16 +13,17 @@ import java.util.List;
 public class ReaderJsonService {
 
     public static List<ContaFacebook> buscarUsuario() {
-        ContaFacebook contaFacebook = new ContaFacebook();
         List<ContaFacebook> contas = Lists.newArrayList();
         ObjectMapper mapper = new ObjectMapper();
         try {
-            File f = new File("contas.json");
-//            contaFacebook = mapper.readValue(f, ContaFacebook.class);
-            contas = mapper.readValue(f, mapper.getTypeFactory().constructCollectionType(List.class, ContaFacebook.class));
-//            List<ContaFacebook> contas = mapper.readValue(f,
-//                    new TypeReference<List<ContaFacebook>>() {
-//                    });
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("./../"));
+            int result = fileChooser.showOpenDialog(null);
+
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = fileChooser.getSelectedFile();
+                contas = mapper.readValue(selectedFile, mapper.getTypeFactory().constructCollectionType(List.class, ContaFacebook.class));
+            }
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         } catch (IOException e) {
