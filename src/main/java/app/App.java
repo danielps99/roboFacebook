81387376197579cceaece14966c9205fb5df1ab6
Daniel <danielps99@gmail.com>
2021-04-18 18:12:32 -0300

    inicio metodo compartilhar

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -30,6 +30,35 @@ public class App implements ICommons {
         percorrerRecursos(contaFacebook.getRecursos());
     }
 
+    private void compartilhar() {
+        driver.get("https://www.facebook.com/story.php?story_fbid=151392173553983&id=101008055259062");
+        sleep(5);
+        WebElement element = driver.findElement(By.xpath("//div[@aria-label='Envie isso para amigos ou publique na sua linha do tempo']"));
+
+        element.click();
+        sleep(5);
+        WebElement element2 = driver.findElement(By.xpath("//*[@class=\"o8rfisnq j83agx80 cbu4d94t tvfksri0 aov4n071 bi6gxh9e l9j0dhe7\"]"));
+        element2.click();
+        sleep(5);
+//        WebElement element3 = driver.findElement(By.xpath("//*[@class=\"hu5pjgll lzf7d6o1 sp_nfy-8GYyygN sx_ec566c\"]"));
+        WebElement element3 = driver.findElement(By.xpath("//*[@class=\"hu5pjgll lzf7d6o1 sp_rngbGqpmQgW sx_00f5db\"]"));
+        println("VAI" + element3.getText());
+        element3.click();
+        sleep(5);
+        WebElement element4 = driver.findElement(By.xpath("//*[@class=\"d2edcug0 hpfvmrgz qv66sw1b c1et5uql b0tq1wua a8c37x1j keod5gw0 nxhoafnm aigsh9s9 d9wwppkn fe6kdd0r mau55g9w c8b282yb hrzyx87i jq4qci2q a3bd9o3v ekzkrbhg oo9gr5id hzawbc8m\"]"));
+        element4.click();
+
+//  escolher qual grupo compartilhar
+
+        WebElement wePublicacoes = driver.findElement(By.xpath("//*[@class=\"rq0escxv l9j0dhe7 du4w35lb j83agx80 cbu4d94t buofh1pr tgvbjcpo muag1w35 enqfppq2\"]"));
+        List<WebElement> elements = wePublicacoes.findElements(By.tagName("div"));
+
+        System.out.println("wePublicacoes"+wePublicacoes.getSize());
+        System.out.println("ELEMENTES"+elements.size());
+
+
+        //Envie isso para amigos ou publique na sua linha do tempo
+    }
     private void inicializarNavegador() {
         Map<String, Object> prefs = new HashMap<>();
         prefs.put("profile.default_content_setting_values.notifications", 2);
